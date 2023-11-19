b2cc9b2e46f5586b5ad813983fee0c0b8c5e18cc
Mohammad Ahmad <mhmad-ahmd@outlook.com>
2023-11-19 12:15:31 +0300

    Formatting

--- resources/js/Pages/Item/Create.vue
+++ resources/js/Pages/Item/Create.vue
@@ -2,7 +2,6 @@
 import InputError from "@/Components/InputError.vue";
 import InputLabel from "@/Components/InputLabel.vue";
 import TextInput from "@/Components/TextInput.vue";
-import Select from "@/Components/Select.vue";
 import PrimaryButton from "@/Components/PrimaryButton.vue";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import { Head, useForm } from "@inertiajs/vue3";
